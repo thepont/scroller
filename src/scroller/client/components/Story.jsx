8d2dc083db8f3da7c7c1f07540783a157de3f393
Paul Esson <paul.esson@blueflag.com.au>
2016-02-14 16:33:42 +1100

    more images and start

--- src/scroller/client/components/Story.jsx
+++ src/scroller/client/components/Story.jsx
@@ -23,35 +23,56 @@ import React from 'react';
 import {ScrollLayer} from './ScrollLayer';
 
 const PageDetails = {
-    tallouse: {
+    start: {
         locationX: 0,
-        previous: '/story/tallouse',
-        next: '/story/grenoble'
+        next: '/story/tallouse',
+        text: ''
+    },
+    tallouse: {
+        locationX: 1,
+        next: '/story/grenoble',
+        text: 'Marie lived in Tallouse, She fed on cheese and wine,'
     },
     grenoble: {
         locationX: 9600,
         previous: '/story/tallouse',
-        next: '/story/duck1'
+        next: '/story/duck1',
+        text: 'Originally from Grenoble, but moved to see what she could find.'
     },
     duck1: {
         locationX: 10300,
         previous: '/story/grenoble',
-        next: '/story/duck2'
+        next: '/story/duck2',
+        text: 'Monsanto corp genetically engeered a her an elephant sized duck,'
     },
     duck2: {
         locationX: 12000,
         previous: '/story/duck1',
-        next: '/story/melbourne'
+        next: '/story/melbourne',
+        text: 'She flew it cross the world, her mount certainly did not suck'
     },
     melbourne: {
         locationX: 12001,
         previous: '/story/duck2',
-        next: '/story/outback'
+        next: '/story/outback',
+        text: 'She arrived in "old" Melboune town, the land of brillant coffee'
     },
     outback: {
         locationX: 14500,
         previous: '/story/melbourne',
-        next: '/story/tallouse'
+        next: '/story/end1',
+        text: 'And drove cross country, befrended a wallaby named hoppy'
+    },
+    end1: {
+        locationX: 15000,
+        previous: '/story/outback',
+        next: '/story/end2',
+        text: 'Hoppy and her lived all happy'
+    },
+    end2: {
+        locationX: 16001,
+        previous: '/story/end1',
+        text: 'And she hang around with coding Paul, Je t\'amie he said to her.'
     }
 }
 
@@ -72,46 +93,55 @@ export class Story extends React.Component {
   componentDidMount(){
       window.addEventListener('scroll', this.handleScroll);
   }
+  renderNav(){
+      
+  }
   render() {
     if(this.props.params){
         var {id} = this.props.params;
     }
     var style = {
         height: this.props.height,
-        backgroundColor: 'black'
+        backgroundColor: 'black',
+        color:'white',
+       textShadow:" -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000"
     }
     var fixedStyle = {
         position: 'fixed',
         top: 'calc(50% - 257px)',
-        height: '514px',
+        // height: ,
         width: '100%',
-    }
-    var centerStyle = {
-        position: 'absolute',
-        
     }
     var skyBlue = {
          backgroundColor: '#7EC0EE',
-         height: '100%',
+         height: '514px',
          width: '100%'
     }
     
-    var pageDetails = PageDetails[id] || PageDetails['tallouse'];
+    var pageDetails = PageDetails[id] || PageDetails['start'];
     
-    return <div style={style} >
+    var nextNav = pageDetails.next ? <Link to={pageDetails.next}>Next</Link> : null;
+    var previousNav = pageDetails.previous ? <Link to={pageDetails.previous}>Previous</Link> : null;
+    
+    return <div style={style}>
+                <span style={{width:'100%', zIndex:'20',fontSize:'2em', padding:'0.5em'}}>{pageDetails.text}</span>
                 <div style={fixedStyle}>
+                    
                     <div style={skyBlue}>
-                        <ScrollLayer zIndex={0} step={-0.08} currentScroll={pageDetails.locationX} width={3228}  image={'/images/mountain.png'} startX={0} stopX={PageDetails.grenoble.locationX}/>
-                        <ScrollLayer zIndex={1} step={-0.1} currentScroll={pageDetails.locationX} width={2221} image={'/images/bg1.png'} startX={0} stopX={PageDetails.grenoble.locationX}/>
-                        <ScrollLayer zIndex={2} step={-0.17} currentScroll={pageDetails.locationX} width={3228} image={'/images/rail.png'} startX={0} stopX={PageDetails.grenoble.locationX}/>
-                        <ScrollLayer zIndex={3} step={-0.102} currentScroll={pageDetails.locationX} width={2221} image={'/images/train.png'} startX={0} stopX={PageDetails.grenoble.locationX}/>
-                        <ScrollLayer zIndex={4} step={-0.2} currentScroll={pageDetails.locationX} width={3228} image={'/images/fg1.png'} startX={0} stopX={PageDetails.grenoble.locationX}/>
-                        <ScrollLayer zIndex={4} step={-0.228} currentScroll={pageDetails.locationX} width={3228} image={'/images/fg2.png'} startX={0} stopX={PageDetails.grenoble.locationX}/>
+                        <ScrollLayer zIndex={0} step={-0.5} currentScroll={pageDetails.locationX} width={'100%'} image={'/images/clouds1.png'} startX={0} stopX={0}/>
+                        <ScrollLayer zIndex={5} step={-0.03} currentScroll={pageDetails.locationX} width={516}  image={'/images/start.png'} startX={0} stopX={0}/>
+                        
+                        <ScrollLayer zIndex={0} step={-0.08} currentScroll={pageDetails.locationX} width={3228}  image={'/images/mountain.png'} startX={PageDetails.tallouse.locationX} stopX={PageDetails.grenoble.locationX}/>
+                        <ScrollLayer zIndex={1} step={-0.1} currentScroll={pageDetails.locationX} width={2221} image={'/images/bg1.png'} startX={PageDetails.tallouse.locationX} stopX={PageDetails.grenoble.locationX}/>
+                        <ScrollLayer zIndex={2} step={-0.17} currentScroll={pageDetails.locationX} width={3228} image={'/images/rail.png'} startX={PageDetails.tallouse.locationX} stopX={PageDetails.grenoble.locationX}/>
+                        <ScrollLayer zIndex={3} step={-0.102} currentScroll={pageDetails.locationX} width={2221} image={'/images/train.png'} startX={PageDetails.tallouse.locationX} stopX={PageDetails.grenoble.locationX}/>
+                        <ScrollLayer zIndex={4} step={-0.2} currentScroll={pageDetails.locationX} width={3228} image={'/images/fg1.png'} startX={PageDetails.tallouse.locationX} stopX={PageDetails.grenoble.locationX}/>
+                        <ScrollLayer zIndex={4} step={-0.228} currentScroll={pageDetails.locationX} width={3228} image={'/images/fg2.png'} startX={PageDetails.tallouse.locationX} stopX={PageDetails.grenoble.locationX}/>
                         
                         
-                        <ScrollLayer zIndex={0} step={-0.5} currentScroll={pageDetails.locationX} width={3000} image={'/images/clouds1.png'} startX={9601} stopX={12000}/>
+                        <ScrollLayer zIndex={0} step={-0.5} currentScroll={pageDetails.locationX} width={4000} image={'/images/clouds1.png'} startX={9601} stopX={12000}/>
                         <ScrollLayer zIndex={1} step={-0.1} currentScroll={pageDetails.locationX} width={667} image={'/images/duck1.png'} startX={10000} stopX={10300}/>
-                        <ScrollLayer zIndex={3} step={-1.2} currentScroll={pageDetails.locationX} width={3000} image={'/images/clouds2.png'} startX={9601} stopX={12000}/>
+                        <ScrollLayer zIndex={3} step={-1.2} currentScroll={pageDetails.locationX} width={4000} image={'/images/clouds2.png'} startX={9601} stopX={12000}/>
                         <ScrollLayer zIndex={1} step={-0.01} currentScroll={pageDetails.locationX} width={731} image={'/images/duck2.png'} startX={10301} stopX={12000}/>
                         
                         <ScrollLayer zIndex={0} step={-0.3} currentScroll={pageDetails.locationX} width={900} image={'/images/melbounesky.png'} startX={12001} stopX={14500}/>
@@ -121,10 +151,18 @@ export class Story extends React.Component {
                         <ScrollLayer zIndex={4} step={-0.54} currentScroll={pageDetails.locationX} width={900} image={'/images/flinders.png'} startX={12001} stopX={14500}/>
                         <ScrollLayer zIndex={5} step={0.02} currentScroll={pageDetails.locationX} width={3400} image={'/images/car.png'} startX={12001} stopX={14500}/>
                         
+                        <ScrollLayer zIndex={3} step={-1.5} currentScroll={pageDetails.locationX} width={2000} image={'/images/wallaby.png'} startX={14501} stopX={15000}/>
+                        <ScrollLayer zIndex={2} step={-2.3} currentScroll={pageDetails.locationX} width={2000} image={'/images/marie-end.png'} startX={14501} stopX={15000}/>
+                        
+                        <ScrollLayer zIndex={3} step={-1.2} currentScroll={pageDetails.locationX} width={2000} image={'/images/paul-end.png'} startX={15001} stopX={16001}/>
                     </div>
                     
-                <Link to={pageDetails.previous}>Previous</Link> 
-                <Link to={pageDetails.next}>Next</Link>        
+                    <div style={{width:'100%', zIndex:'20'}}>
+                        {previousNav}
+                        {nextNav}
+                    </div>
+                    
+                    
                 </div>
                 
         </div>
