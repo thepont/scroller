94608356694fd3f3b6da17f274c852aae9bcf8f7
Paul Esson <paul.esson@gmail.com>
2016-02-16 15:05:03 +1100

    Update Story.jsx
    Remove unused code

--- src/scroller/client/components/Story.jsx
+++ src/scroller/client/components/Story.jsx
@@ -1,25 +1,4 @@
 import React from 'react';
-
-// export const Story = React.createClass({
-//     getInitialState: function() {
-//         return {scrollY: 0};
-//     },
-//     handleScroll: () => {
-//         console.log('hello', window.scrollY)
-//         this.setState({
-//             scrollY:0
-//         })
-//     },
-//     componentDidMount: () => {
-//         window.addEventListener('scroll', this.handleScroll);
-//     },
-//     render() {
-//         var style = {
-//             height: this.props.height
-//         }
-//         return <div style={style}>Hello {this.state.scrollY}</div>;
-//     }  
-// });
 import {ScrollLayer} from './ScrollLayer';
 
 const PageDetails = {
@@ -81,17 +60,7 @@ import {Link} from 'react-router';
 export class Story extends React.Component {
   constructor(props) {
     super(props);
-    this.state={scrollY:0};
     this.handleScroll = this.handleScroll.bind(this);
-  }
-  handleScroll(){
-    console.log(window.scrollY);
-    this.setState({
-          scrollY:window.scrollY
-    });
-  }
-  componentDidMount(){
-      window.addEventListener('scroll', this.handleScroll);
   }
   renderNav(){
       
